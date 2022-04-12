174ff6ee51ceebfc91e49428d413dd78632ebfe4
gzyabrov <gzyabrov@rvision.pro>
2022-04-12 20:46:07 +0300

    delete import

--- src/test/java/ru/nspk/tests/TestBase.java
+++ src/test/java/ru/nspk/tests/TestBase.java
@@ -3,7 +3,7 @@ package ru.nspk.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import io.restassured.RestAssured;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
